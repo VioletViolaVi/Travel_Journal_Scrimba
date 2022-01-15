7d3a86d3e87c2efe2de8253528e6ef133d6099ac
vivian <vivian.v18723@gmail.com>
2022-01-15 23:21:39 +0000

    Add more data to its file and document in readme.

--- travel-journal-app/src/components/data/Data.js
+++ travel-journal-app/src/components/data/Data.js
@@ -13,5 +13,47 @@ const Data = [
     paragraph:
       "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
   },
+  {
+    id: 2,
+    imageLink:
+      "https://images.unsplash.com/photo-1499359875449-10bbeb21501e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    image: "singapore.jpg",
+    altAndTitle: "Gardens by the Bay - Supertree Grove",
+    countryName: "singapore".toUpperCase(),
+    hrefMapLInk:
+      "https://www.google.com/maps?q=Supertree+Grove&sxsrf=AOaemvIyV8tFVtXvj5ZBNNlbVWIFZLqdBg:1642287270593&uact=5&gs_lcp=Cgdnd3Mtd2l6EAMyBAgAEEcyBAgAEEcyBAgAEEcyBAgAEEcyBAgAEEcyBAgAEEcyBAgAEEcyBAgAEEdKBAhBGABKBAhGGABQpAZYpAZgyQZoAHADeACAAQCIAQCSAQCYAQCgAQHIAQjAAQE&um=1&ie=UTF-8&sa=X&ved=2ahUKEwjts7Ht7LT1AhX4Q0EAHbg7B9wQ_AUoAnoECAIQBA",
+    startDate: "01 December, 2020",
+    endDate: "01 April 2027",
+    paragraph:
+      "The Gardens by the Bay is a nature park spanning 101 hectares (250 acres) in the Central Region of Singapore, adjacent to the Marina Reservoir. The park consists of three waterfront gardens: Bay South Garden (in Marina South), Bay East Garden (in Marina East) and Bay Central Garden (in Downtown Core and Kallang). The largest of the gardens is the Bay South Garden at 54 hectares (130 acres) designed by Grant Associates. Its Flower Dome is the largest glass greenhouse in the world.",
+  },
+  {
+    id: 3,
+    imageLink:
+      "https://images.unsplash.com/photo-1518638150340-f706e86654de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=867&q=80",
+    image: "pyramid.jpg",
+    altAndTitle: "Pyramid of the Sun",
+    countryName: "mexico".toUpperCase(),
+    hrefMapLInk:
+      "https://www.google.com/maps/place/Pyramid+of+the+Sun/@19.6922325,-98.8457566,17z/data=!4m5!3m4!1s0x85d1ea845eb30ff9:0xad84278bbe6d0c38!8m2!3d19.6922729!4d-98.8435035",
+    startDate: "18 March, 2021",
+    endDate: "24 October, 2024",
+    paragraph:
+      "The Pyramid of the Sun is the largest building in Teotihuacan, and one of the largest in Mesoamerica. It is believed to have been constructed about 200 AD. Found along the Avenue of the Dead, in between the Pyramid of the Moon and the Ciudadela, and in the shadow of the mountain Cerro Gordo, the pyramid is part of a large complex in the heart of the city.",
+  },
+  {
+    id: 4,
+    imageLink:
+      "https://images.unsplash.com/photo-1545853332-147d5073187e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    image: "london-eye.jpg",
+    altAndTitle: "The London Eye",
+    countryName: "london".toUpperCase(),
+    hrefMapLInk:
+      "https://www.google.com/maps?q=London+Eye&um=1&ie=UTF-8&sa=X&ved=2ahUKEwjw76-38rT1AhVEi1wKHSGkCeUQ_AUoAnoECAIQBA",
+    startDate: "30 November 2022",
+    endDate: "16 July 2030",
+    paragraph:
+      "The London Eye, or the Millennium Wheel, is a cantilevered observation wheel on the South Bank of the River Thames in London. It is Europe's tallest cantilevered observation wheel, and is the most popular paid tourist attraction in the United Kingdom with over 3 million visitors annually. It has made many appearances in popular culture.",
+  },
 ];
 export default Data;
